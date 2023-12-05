2e4ca3357b90214ab3aec3f6fa94711c06ddf5a7
dongjun <allnight724@gmail.com>
2023-12-05 14:13:48 +0900

    Firebase Project

---
+++ twitter-reloaded/src/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyArP70QejPBF9TicJ09ygq4G5s0xmJO--0",
+  authDomain: "twitter-reloaded-e300f.firebaseapp.com",
+  projectId: "twitter-reloaded-e300f",
+  storageBucket: "twitter-reloaded-e300f.appspot.com",
+  messagingSenderId: "512696930540",
+  appId: "1:512696930540:web:c6214e11f7c1dc806fd5c9"
+};
+
+const app = initializeApp(firebaseConfig);
