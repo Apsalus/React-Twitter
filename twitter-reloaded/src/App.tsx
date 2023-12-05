5e031013ec50f575592a89f0e0a624808bc4064a
dongjun <allnight724@gmail.com>
2023-12-06 00:47:17 +0900

    Github Login/Create Account

--- twitter-reloaded/src/App.tsx
+++ twitter-reloaded/src/App.tsx
@@ -4,15 +4,21 @@ import Home from "./routes/home";
 import Profile from "./routes/profile";
 import Login from "./routes/login";
 import CreateAccount from "./routes/create-account";
-import { createGlobalStyle } from "styled-components";
+import { createGlobalStyle, styled } from "styled-components";
 import reset from "styled-reset";
 import { useEffect, useState } from "react";
 import LoadingScreen from "./components/loading-screen";
+import { auth } from "./firebase";
+import ProtectedRoute from "./components/protected-route";
 
 const router = createBrowserRouter([
   {
   path:"/",
-  element: <Layout />,
+  element: (
+    <ProtectedRoute>
+      <Layout />
+    </ProtectedRoute>
+  ),
   children: [
     {
       path: "",
@@ -47,19 +53,26 @@ const GlobalStyles = createGlobalStyle`
     sans-serif;
   }
 `
+const Wrapper = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+
+`
 
 function App() {
   const [isLoading, setIsLoding] = useState(true);
   const init = async () => {
+    await auth.authStateReady();
     setIsLoding(false);
   };
   useEffect(() => {
     init();
   }, []);
-    return( <>
+    return( <Wrapper>
     <GlobalStyles />
     {isLoading ? <LoadingScreen /> : <RouterProvider router={router} />}
-  </>
+  </Wrapper>
   );
 }
 
--- twitter-reloaded/src/firebase.ts
+++ twitter-reloaded/src/firebase.ts
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyArP70QejPBF9TicJ09ygq4G5s0xmJO--0",
@@ -10,3 +11,5 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
