8b647f86d2936e5f466d05dedd81f518624e62f9
dongjun <allnight724@gmail.com>
2023-12-06 16:56:00 +0900

    Post a tweet to Firebase and Uploading Images

--- twitter-reloaded/src/firebase.ts
+++ twitter-reloaded/src/firebase.ts
@@ -1,5 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyArP70QejPBF9TicJ09ygq4G5s0xmJO--0",
@@ -13,3 +15,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
+
+export const storage = getStorage(app);
+
+export const db = getFirestore(app);
--- twitter-reloaded/src/routes/login.tsx
+++ twitter-reloaded/src/routes/login.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { auth } from "../firebase";
 import { Link, useNavigate } from "react-router-dom";
 import { FirebaseError } from "firebase/app";
-import { signInWithEmailAndPassword } from "firebase/auth";
+import { sendPasswordResetEmail, signInWithEmailAndPassword } from "firebase/auth";
 import {
   Error,
   Form,
